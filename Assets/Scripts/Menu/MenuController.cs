b4214a78f6cf8dec9088a38e41ef97fa0dbb5e48
Alumno-J <Alumno-J>
2016-06-07 22:14:03 -0500

    django

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -8,10 +8,30 @@ public class MenuController : MonoBehaviour {
     // Use this for initialization
     public Button btnPlay;
 	void Start () {
+      
+        StartCoroutine(saveDeviceData());
+
         btnPlay.onClick.AddListener(() => PlayGame());
 	}
 	
 	// Update is called once per frame
+    IEnumerator saveDeviceData()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("uid", SystemInfo.deviceUniqueIdentifier);
+        form.AddField("device_model", SystemInfo.deviceModel);
+        form.AddField("device_name", SystemInfo.deviceName);
+        WWW url = new WWW("https://prototipadoclase11-fortune9316.c9users.io/prueba/",form);
+        yield return url;
+        if(url.error == null)
+        {
+            print(url.text);
+        }
+        else
+        {
+            print(url.error);
+        }
+    }
 	void Update () {
 	
 	}
